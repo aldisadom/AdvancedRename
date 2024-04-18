8efd3fd13c3cddad2b5a86575e39a7ce91fca1b9
Aldis Adomavičius <aldisadom@gmail.com>
2024-04-18 08:31:22 +0300

    Add project files.

---
+++ AdvancedRenamer/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AdvancedRenamer;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Renamer started");
+        Configuration configuration = new()
+        {
+            Template = TemplateName.VLVD,
+            Generate = true
+        };
+
+        RenamerService service = new RenamerService(configuration);
+
+        service.Rename();
+
+        Console.WriteLine("Renamer finished");
+    }
+}
